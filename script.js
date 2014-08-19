2700a64db9366675939446591d651aedbffee0ce
Dustin Yu <dustin520@gmail.com>
2014-08-19 02:20:13 -0700

    added some animations to images

---
+++ script.js
@@ -0,0 +1,75 @@
+$(function () {
+
+	$("#oneOne").on('click', function() {
+		$('#oneOne').fadeOut(500).fadeIn(500);
+	});
+
+	$("#oneTwo").on('mouseover', function() {
+		$('#oneTwo').slideUp(400).slideDown(400);
+	});
+
+	$("#oneThree").on('click', function() {
+		$('#oneThree').fadeOut(400).fadeIn(400);
+	});
+
+	$("#twoOne").on('click', function() {
+		$('#twoOne').slideUp(500).slideDown(500);
+	});
+
+	$("#twoTwo").on('click', function() {
+		$('#twoTwo').slideUp(600).slideDown(600);
+	});
+
+	$("#twoThree").on('click', function() {
+		$('#twoThree').fadeOut(500).fadeIn(500);
+	});
+
+	$("#threeOne").on('click', function() {
+		$('#threeOne').fadeOut(500).fadeIn(500);
+	});
+
+	$("#threeTwo").on('mouseover', function() {
+		$('#threeTwo').fadeOut(500).fadeIn(500);
+	});
+
+	$("#threeThree").on('click', function() {
+		$('#threeThree').slideUp(350).fadeIn(350);
+	});
+
+	$("#fourOne").on('click', function() {
+		$('#fourOne').slideUp(350).slideDown(500);
+	});
+
+	$("#fourTwo").on('click', function() {
+		$('#fourTwo').fadeOut(500).fadeIn(500);
+	});
+
+	$("#fourThree").on('mouseover', function() {
+		$('#fourThree').fadeOut(420).fadeIn(420);
+	});
+
+	$("#fiveOne").on('click', function() {
+		$('#fiveOne').fadeOut(420).fadeIn(500);
+	});
+
+	$("#fiveTwo").on('mouseover', function() {
+		$('#fiveTwo').slideUp(500).slideDown(500);
+	});
+
+	$("#fiveThree").on('click', function() {
+		$('#fiveThree').slideDown(500).slideUp(500);
+	});
+
+	$("#sixOne").on('mouseover', function() {
+		$('#sixOne').slideUp(500).slideDown(500);
+	});
+
+	$("#sixTwo").on('click', function() {
+		$('#sixTwo').fadeOut(500).fadeIn(500);
+	});
+
+	$("#sixThree").on('click', function() {
+		$('#sixThree').slideUp(500).slideDown(500);
+	});
+
+});
